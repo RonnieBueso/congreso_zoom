ade2116235cbd0e52c2841f7b2d2f3985acb61f2
Ronnie Bueso <168491371+RonnieBueso@users.noreply.github.com>
2025-07-23 11:42:12 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY_AQUI",
+  apiKey: "AIzaSyABt2aXGvlcP8Pyy-MEr2kZbHurcMuPX1I",
   authDomain: "congreso2025-6be09.firebaseapp.com",
   databaseURL: "https://congreso2025-6be09-default-rtdb.firebaseio.com",
   projectId: "congreso2025-6be09",
-  storageBucket: "congreso2025-6be09.appspot.com",
+  storageBucket: "congreso2025-6be09.firebasestorage.app",
   messagingSenderId: "334410660172",
-  appId: "TU_APP_ID_AQUI"
+  appId: "1:334410660172:web:6c662660d39ed4346677e9",
+  measurementId: "G-Y485YY6XW9"
 };
